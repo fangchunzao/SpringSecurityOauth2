f6f1f865f966a4b9ed1385fe0d66553cd164493b
fangchunzao <31404242+fangchunzao@users.noreply.github.com>
2018-06-29 10:06:10 +0800

    Update ClientApplication.java

--- src/main/java/oauth/security/client/ClientApplication.java
+++ src/main/java/oauth/security/client/ClientApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-/**
- * Created by xujingfeng on 2017/8/7.
- */
 @SpringBootApplication
 @ServletComponentScan
 public class ClientApplication {
